1fafc27a422701deee1b1286147715ff7e3311e9
Agustin <agus_ph_2@outlook.com>
2024-01-16 21:36:48 -0300

    nuevo

--- temporizador/main.js
+++ temporizador/main.js
@@ -1,20 +1,50 @@
 var horasInput = document.getElementById("horas");
 var minutosInput = document.getElementById("minutos");
 var segundosInput = document.getElementById("segundos");
-var elementoTiempo = document.getElementById("tiempo");
+var elementoTiempo = document.getElementById("reloj");
+var tiempo;
+var intervalo;
 
 function actualizarTiempo() {
   var horas = horasInput.value;
   var minutos = minutosInput.value;
   var segundos = segundosInput.value;
 
+  tiempo = horas * 3600 + minutos * 60 + segundos;
+
   elementoTiempo.textContent =
     (horas < 10 ? "0" + horas : horas) +
     ":" +
     (minutos < 10 ? "0" + minutos : minutos) +
     ":" +
-    (segundos < 10 ? "0" + segundos : segundos) +
-    ":";
+    (segundos < 10 ? "0" + segundos : segundos);
+}
+
+function iniciarTiempo() {
+  actualizarTiempo();
+
+  intervalo = setInterval(function () {
+    tiempo--;
+
+    var horas = Math.floor(tiempo / 3600);
+    var minutos = Math.floor((tiempo % 3600) / 60);
+    var segundos = tiempo % 60;
+
+    elementoTiempo.textContent =
+      (horas < 10 ? "0" + horas : horas) +
+      ":" +
+      (minutos < 10 ? "0" + minutos : minutos) +
+      ":" +
+      (segundos < 10 ? "0" + segundos : segundos);
+
+    if (tiempo <= 0) {
+      clearInterval(intervalo);
+    }
+  }, 1000);
 }
 
 horasInput.addEventListener("input", actualizarTiempo);
+minutosInput.addEventListener("input", actualizarTiempo);
+segundosInput.addEventListener("input", actualizarTiempo);
+
+document.getElementById("inicio").addEventListener("click", iniciarTiempo);
